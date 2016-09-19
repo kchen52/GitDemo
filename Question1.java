972c813e4483ada1722f12dfc44a02f523caa5f1
Kevin Chen <k.chen52@gmail.com>
2016-09-18 19:16:28 -0700

    Initial commit.

---
+++ Question1.java
@@ -0,0 +1,9 @@
+/*
+ * Assignment: Have the program output "Hello from <name>" for each group member.
+ */
+
+class Question1 {
+    public static void main(String args[]) {
+        System.out.println("How about no.");
+    }
+}
